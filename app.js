ff798f346a950c258f235b27d1ad82045777041c
Duncan <Duncan.David@gmail.com>
2015-12-29 00:22:21 -0500

    added nickname support, css not changed yet

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 3000;
+
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  var id = socket.id;
+
   socket.on('chat message', function(msg) {
+    var id = socket.nickname ? socket.nickname : socket.id;
     console.log('message from' + id + ': ' + msg);
     io.emit('chat message', 'message from ' + id + ': ' + msg);
     io.emit('server message', "message from server on port " + port + ": thanks for the heartfelt message, " + id + "!");
+  });
+
+  socket.on('nickname change', function(data){
+    socket.nickname = data;
+    console.log(socket.adapter.nsp.connected);
   });
 
   socket.on('disconnect', function(){
